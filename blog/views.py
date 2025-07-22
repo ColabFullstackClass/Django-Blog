38252853f35c2871ee8c8412584020a656872282
Edoka Isaac <edokaizkdaking247@gmail.com>
2025-07-22 14:05:18 +0100

    Created views, set up templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from .models import Post
+from django.http import Http404
+#from django.views.generic import ListView
 
 # Create your views here.
+
+# Function-Based Views
+# Class-Based Views
+# class PostView(ListView):
+
+def post_list(request):
+    """
+    View Function To Return Posts
+    """
+    posts = Post.published.all()
+    context = {'posts': posts}
+    return render(request, 'post/list.html', context)
+
+def post_detail(request, id):
+    # try:
+    #     post = Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404("Post Not Found")
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    
+    context = {'post': post}
+    return render(request, 'post/detail.html', context)
